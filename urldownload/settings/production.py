cb458c3a113ab6b364ff486978eb270653f699da
tvishnu <tvishnu@zeomega.com>
2019-08-14 10:03:41 +0530

    celery settings

--- urldownload/settings/production.py
+++ urldownload/settings/production.py
@@ -34,7 +34,7 @@ EMAIL_HOST_USER = 'djangosmtpemail@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-BROKER_URL = 'amqp://eyetwodm:ChVMhEyKNbCDSkD5BcmuM9JgBJVok7yB@termite.rmq.cloudamqp.com/eyetwodm'
+BROKER_URL = 'amqp://jccxepey:F9kIxDojYuvGB7mIoS9A_rmAQMhswq1e@termite.rmq.cloudamqp.com/jccxepey'
 BROKER_POOL_LIMIT = 1
 imports = ('urlemail.api.tasks',)
 # BROKER_HEARTBEAT = None
